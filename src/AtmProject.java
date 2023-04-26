96f2ed68bcae115b59ee738bc0644355438ff750
meconl <meconl@icloud.com>
2023-04-26 21:02:57 +0200

    v4

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -6,6 +6,8 @@ public class AtmProject {
         //version2
         System.out.println("Learn java earn money");
         //version3
+        System.out.println("Git ogrenmek cok kolay");
+        //version4
     }
 
 
